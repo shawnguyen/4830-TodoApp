fad382951d5f7d3983bb3d978a288331c3748730
Shawn Nguyen <shawnnguyen@mwc-088101.dhcp.missouri.edu>
2020-09-25 07:31:34 -0500

    Added animations and the angular animations module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,24 @@
 import { Component, ViewChild } from '@angular/core';
+import { trigger, state, style, animate, transition, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations:[
+      trigger("moveInLeft",[
+       transition("void=> *",[style({transform:"translateX(300px)"}),
+        animate(200,keyframes([
+         style({transform:"translateX(300px)"}),
+         style({transform:"translateX(0)"})
+       ]))]),
+       transition("*=>void",[style({transform:"translateX(0px)"}),
+        animate(100,keyframes([
+         style({transform:"translateX(0px)"}),
+         style({transform:"translateX(300px)"})
+        ]))]) 
+       ])
+      ],
 })
 export class AppComponent {
   title = 'todo-app';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 import { AppComponent } from './app.component';
@@ -11,7 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
